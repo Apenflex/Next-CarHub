9239b53638088639400c0f20473cdb356d4ee69a
Svyatoslav <blackdbf@gmail.com>
2023-08-07 19:03:40 +0300

    fix: remove variables to env file

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
 export async function fetchCars() {
 	const headers = {
-		'X-RapidAPI-Key': 'e22248be1dmsh23609edc7fe99f1p1092b7jsn6c3e816f0edf',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+		'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+		'X-RapidAPI-Host': process.env.RAPID_API_HOST,
 	}
 	const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', { method: 'GET', headers })
   console.log(response)
