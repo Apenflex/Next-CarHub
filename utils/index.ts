d61c3ce66d77f121045f8211c5ad39f21d8d9ddc
Svyatoslav Ivanyshyn <98613056+Apenflex@users.noreply.github.com>
2023-08-07 19:02:56 +0300

    fix: delete variables from index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
 export async function fetchCars() {
 	const headers = {
-		'X-RapidAPI-Key': 'e22248be1dmsh23609edc7fe99f1p1092b7jsn6c3e816f0edf',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+		'X-RapidAPI-Key': '',
+		'X-RapidAPI-Host': '',
 	}
 	const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', { method: 'GET', headers })
   console.log(response)
