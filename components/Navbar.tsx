4181bfc7c5b4923a190f1313315c27ac7ab6c985
Svyatoslav <blackdbf@gmail.com>
2023-08-10 00:02:43 +0300

    feat: add link to header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { CustomButton } from './'
+import { HeaderLinks } from './'
 
 const Navbar = () => {
 	return (
-		<header className="absolute z-10 w-full">
-			<nav className="mx-auto flex max-w-[1440px] items-center justify-between px-6 py-4 sm:px-16">
+		<header className="header">
+			<nav className="nav">
 				<Link href="/" className="flex items-center justify-center">
 					<Image src="/logo.svg" alt="Car Hub Logo" width={118} height={18} className="object-contain" />
 				</Link>
-				<CustomButton
-					title="Sign In"
-					btnType="button"
-					containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
-				/>
+				<HeaderLinks />
 			</nav>
 		</header>
 	)
--- components/index.ts
+++ components/index.ts
@@ -3,9 +3,10 @@ import CarDetails from './CarDetails'
 import CustomButton from './CustomButton'
 import CustomFilter from './CustomFilter'
 import Footer from './Footer'
+import HeaderLinks from './HeaderLinks'
 import Hero from './Hero'
 import Navbar from './Navbar'
 import SearchBar from './SearchBar'
 import SearchManufacturer from './SearchManufacturer'
 
-export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer, CarCard, CarDetails }
+export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer, CarCard, CarDetails, HeaderLinks }
